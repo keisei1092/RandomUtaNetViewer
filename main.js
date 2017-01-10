3d06f1219d09c7194e635e16f4d818c4fedfec25
keisei_1092 <keisei_1092@yahoo.co.jp>
2017-01-11 00:02:22 +0900

    commit

--- main.js
+++ main.js
@@ -16,14 +16,11 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  const seed = Math.floor( Math.random() * 221900 ) + 1;
+  mainWindow.loadURL('http://www.uta-net.com/song/' + seed);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
